46cf1ee153445f3e9472a7560dbf2603273b18a0
BongoCaat <valentinopietropaolo70@gmail.com>
2024-04-25 07:32:26 -0300

    agregado descuento de 10% y unos arreglos para valores incorrectos

--- JS/alert.js
+++ JS/alert.js
@@ -1,11 +1,25 @@
-let nombreProducto = "Arroz con leche";
+function comprar() {
+    let nombreProducto = "Arroz con leche";
 
-let precioUnitario = 1200;
+    let precioUnitario = 1000;
 
-let cantidadDeseada = parseInt(prompt(`¿Cuántas unidades de ${nombreProducto} desea comprar?`));
+    let cantidadDeseada = parseInt(prompt(`¿Cuántas unidades de ${nombreProducto} desea comprar? Precio: (${precioUnitario}) (si compra más de 5 productos tiene un descuento de 10%)`));
 
-let costoTotal = precioUnitario * cantidadDeseada;
+    if (cantidadDeseada === 0 || isNaN(cantidadDeseada)) {
+        alert("Ingrese una cantidad válida.")
+        return;
+    }
 
-alert(`El costo total de la compra de ${nombreProducto} es de ${costoTotal} pesos.`);
+    let costoTotal = precioUnitario * cantidadDeseada;
 
-console.log( `El costo total de la compra es de ${costoTotal}`);
+    if (cantidadDeseada >= 5) {
+        let descuento = costoTotal / 10;
+        let costoTotalConDescuento = costoTotal - descuento;
+
+        alert(`El costo total de la compra de ${nombreProducto} es de ${costoTotalConDescuento} pesos con un descuento de 10%.`)
+        console.log(`El costo total de la compra es de ${costoTotalConDescuento} pesos.`);
+    } else {
+        alert(`El costo total de la compra de ${nombreProducto} es de ${costoTotal} pesos.`)
+        console.log(`El costo total de la compra es de ${costoTotal} pesos.`);
+    }
+};
