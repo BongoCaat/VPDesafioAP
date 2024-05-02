866fe448110c4a7d1ea4e258aa030ec4749f0d65
BongoCaat <valentinopietropaolo70@gmail.com>
2024-05-02 16:59:39 -0300

    function de sumarproductos

--- JS/alert.js
+++ JS/alert.js
@@ -1,3 +1,9 @@
+function sumarProductos(precioUnitario, cantidadDeseada){
+    let totalGastado = precioUnitario * cantidadDeseada;
+
+    return totalGastado;
+}
+
 function comprar() {
     let nombreProducto = "Arroz con leche";
 
@@ -6,20 +12,21 @@ function comprar() {
     let cantidadDeseada = parseInt(prompt(`¿Cuántas unidades de ${nombreProducto} desea comprar? Precio: (${precioUnitario}) (si compra más de 5 productos tiene un descuento de 10%)`));
 
     if (cantidadDeseada === 0 || isNaN(cantidadDeseada)) {
-        alert("Ingrese una cantidad válida.")
+        alert("Ingrese una cantidad válida.");
+
         return;
     }
 
-    let costoTotal = precioUnitario * cantidadDeseada;
+    costoTotal = sumarProductos(precioUnitario, cantidadDeseada);
 
     if (cantidadDeseada >= 5) {
         let descuento = costoTotal / 10;
         let costoTotalConDescuento = costoTotal - descuento;
 
-        alert(`El costo total de la compra de ${nombreProducto} es de ${costoTotalConDescuento} pesos con un descuento de 10%.`)
-        console.log(`El costo total de la compra es de ${costoTotalConDescuento} pesos.`);
+        alert(`El costo total de la compra de ${nombreProducto} es de ${costoTotalConDescuento} pesos con un descuento de 10%.`);
+        console.log(`El costo total de la compra es de ${costoTotalConDescuento} pesos con un descuento de 10%(${descuento}).`);
     } else {
-        alert(`El costo total de la compra de ${nombreProducto} es de ${costoTotal} pesos.`)
+        alert(`El costo total de la compra de ${nombreProducto} es de ${costoTotal} pesos.`);
         console.log(`El costo total de la compra es de ${costoTotal} pesos.`);
     }
 };
